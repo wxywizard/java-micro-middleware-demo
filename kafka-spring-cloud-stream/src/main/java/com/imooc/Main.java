c6533a94d21b67573e6d501ca86c88a7cca2c991
wangxy <wangxuyang16@crdigital.com.cn>
2023-08-11 17:26:18 +0800

    add base project

---
+++ kafka-spring-cloud-stream/src/main/java/com/imooc/Main.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+/**
+ * @author wangxy
+ * @Classname ${NAME}
+ * @Date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
